b0f88433daf944b60a6a444a6f71ccc730e4599d
Caique Moreira <caique_moreira7@hotmail.com>
2021-01-11 01:42:03 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css';
 import Routes from './Routes';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css'
 
 function App() {
   return (
+    <>
     <Routes/>
+    <ToastContainer/>
+    </>
   );
 }
 
